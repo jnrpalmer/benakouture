7548b7234348ac1eb960d3eac4dd7f223519e736
Palmer <kobbiekyei423@gmaill.com>
2026-04-27 23:34:28 +0000

    fix:update

--- backend/models/Exhibition.js
+++ backend/models/Exhibition.js
@@ -1,9 +1,9 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const ExhibitionSchema = new mongoose.Schema({
   url:  String,
-  type: { type: String, enum: ['image','video'], default: 'image' },
+  type: { type: String, enum: ["image", "video"], default: "image" },
   name: String
 }, { timestamps: true });
 
-module.exports = mongoose.model('Exhibition', ExhibitionSchema);
+module.exports = mongoose.model("Exhibition", ExhibitionSchema);
